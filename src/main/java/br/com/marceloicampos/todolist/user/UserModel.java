b89bf41594e24126d983ee7aff1d8f3380f14e99
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-10 12:53:40 -0300

    Create Entity tb_users with uuid and timestamp

--- src/main/java/br/com/marceloicampos/todolist/user/UserModel.java
+++ src/main/java/br/com/marceloicampos/todolist/user/UserModel.java
@@ -3,12 +3,25 @@ package br.com.marceloicampos.todolist.user;
 // import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 // @Data junta Setter e junta Getter em uma só annotation
+@Entity(name = "tb_users")
 public class UserModel {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
   private String name;
   private String username;
   private String password;
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
