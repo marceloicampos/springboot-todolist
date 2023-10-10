3511f42eb8f338b4053321b59fd2b3a9f99aab76
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-09 22:07:14 -0300

    Create Basic UserModel and UserController

---
+++ src/main/java/br/com/marceloicampos/todolist/user/UserController.java
@@ -0,0 +1,27 @@
+package br.com.marceloicampos.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ ** Modificadores: public, private, protected
+ ** Tipos: class, interface, enum, static, boolean
+ */
+
+@RestController
+@RequestMapping("/users")
+// http://localhost:8080/users/
+public class UserController {
+  /**
+   ** Tipos Primitivos: string (texto), integer (inteiros), double (decimais),
+   * float (decimais), char (caracteres), date (data), void (vazio)
+   */
+  @PostMapping("/")
+  public void create(@RequestBody UserModel UserModel) {
+    System.out.println(UserModel.name);
+    System.out.println(UserModel.username);
+    System.out.println(UserModel.password);
+  }
+}
