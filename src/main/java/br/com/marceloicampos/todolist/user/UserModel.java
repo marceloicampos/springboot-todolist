9ab1996aa1c28123b1762e8386a7e2ad2192c96e
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-10 10:49:43 -0300

    Getters and Setters with Lombok

--- src/main/java/br/com/marceloicampos/todolist/user/UserModel.java
+++ src/main/java/br/com/marceloicampos/todolist/user/UserModel.java
@@ -1,31 +1,14 @@
 package br.com.marceloicampos.todolist.user;
 
+// import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+// @Data junta Setter e junta Getter em uma só annotation
 public class UserModel {
   private String name;
   private String username;
   private String password;
-
-  public String getName() {
-    return name;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
