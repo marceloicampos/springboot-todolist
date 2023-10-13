80b7389bf45adf13e21d85683f5a87df86dfc50c
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-13 18:02:41 -0300

    Create Entity Response

--- src/main/java/br/com/marceloicampos/todolist/user/UserController.java
+++ src/main/java/br/com/marceloicampos/todolist/user/UserController.java
@@ -1,10 +1,10 @@
 package br.com.marceloicampos.todolist.user;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
