6012ae93a03b36b504f45ec7774c1efebf514def
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-13 19:11:41 -0300

    Create New Task with Task Controller

---
+++ src/main/java/br/com/marceloicampos/todolist/task/TaskRepository.java
@@ -0,0 +1,8 @@
+package br.com.marceloicampos.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
