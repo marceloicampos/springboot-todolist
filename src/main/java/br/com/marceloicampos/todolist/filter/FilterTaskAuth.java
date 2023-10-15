a65ac92b6981a3613f659204ca8f3bb29b2db239
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-15 12:44:49 -0300

    Password Decode and Route Validate

--- src/main/java/br/com/marceloicampos/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/marceloicampos/todolist/filter/FilterTaskAuth.java
@@ -1,37 +1,65 @@
 package br.com.marceloicampos.todolist.filter;
 
+import br.com.marceloicampos.todolist.user.UserRepository;
+
 import java.io.IOException;
 import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    // captura o auth, valida user, valida password e segue com o processo
-    var authorization = request.getHeader("Authorization");
-    System.out.println("Auth: " + authorization);
-    var authEncoded = authorization.substring("Basic".length()).trim();
-    byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
-    var authString = new String(authDecoded);
-    String[] credentials = authString.split(":");
-    String username = credentials[0];
-    String password = credentials[1];
 
-    System.out.println("authEncoded: " + authEncoded);
-    System.out.println("authDecoded: " + authDecoded);
-    System.out.println("authString: " + authString);
-    System.out.println("username: " + username);
-    System.out.println("password: " + password);
-    filterChain.doFilter(request, response);
+    var servletPath = request.getServletPath();
+
+    if (servletPath.equals("/tasks")) {
+      // captura o auth, valida user, valida password e segue com o processo
+      var authorization = request.getHeader("Authorization");
+      System.out.println("Auth: " + authorization);
+      var authEncoded = authorization.substring("Basic".length()).trim();
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+      var authString = new String(authDecoded);
+      // marceloicampos, abc123
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+      System.out.println("authEncoded: " + authEncoded);
+      System.out.println("authDecoded: " + authDecoded);
+      System.out.println("authString: " + authString);
+      System.out.println("username: " + username);
+      System.out.println("password: " + password);
+      // validar usuário
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
+        response.sendError(401, "User Not Found");
+        return;
+      } else {
+        // validar a senha
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified == true) {
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401, "Invalid Password");
+        }
+      }
+    } else {
+      filterChain.doFilter(request, response);
+    }
   }
 }
