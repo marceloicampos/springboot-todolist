9cc69cac51768bcf6bec06756269bf730eac98f0
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-17 04:13:22 -0300

    Time Validation

--- src/main/java/br/com/marceloicampos/todolist/task/TaskController.java
+++ src/main/java/br/com/marceloicampos/todolist/task/TaskController.java
@@ -1,14 +1,16 @@
 package br.com.marceloicampos.todolist.task;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.UUID;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,11 +22,24 @@ public class TaskController {
   private TaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     System.out.println("No TaskController " + request.getAttribute("idUser"));
     var idUser = request.getAttribute("idUser");
     taskModel.setIdUser((UUID) idUser);
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Invalid date specified - Current Date is Later than the Start Date");
+    }
+    if (currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Invalid date specified - Current Date is Later than the End Date");
+    }
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Invalid date specified - The Start Date is Later than End Date");
+    }
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   };
 }
