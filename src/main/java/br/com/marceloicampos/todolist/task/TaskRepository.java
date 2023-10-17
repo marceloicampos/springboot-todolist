88ecf259b52f716ec0f4ed3acc746861ed023cc6
marceloicampos <marcelo.ildefonso@hotmail.com>
2023-10-17 08:09:15 -0300

    User List Tasks

--- src/main/java/br/com/marceloicampos/todolist/task/TaskRepository.java
+++ src/main/java/br/com/marceloicampos/todolist/task/TaskRepository.java
@@ -1,8 +1,9 @@
 package br.com.marceloicampos.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
-
+  List<TaskModel> findByIdUser(UUID idUser);
 }
